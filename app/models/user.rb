3b06cb5bf38b92db57c439ebc7a4c8190dc708c4
umeskayame <ume122.0321@gmail.com>
2023-10-01 18:02:10 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: {
+           with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+         },on: :create
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, format: {
+    with: /\A[ぁ-んァ-ヶ一-龥]/
+  },on: :create
+  validates :first_name, format: {
+    with: /\A[ぁ-んァ-ヶ一-龥]/
+  },on: :create
+  validates :kana_last_name, format: {
+    with: /\A[ァ-ヶー－]+\z/
+  },on: :create
+  validates :kana_first_name, format: {
+    with: /\A[ァ-ヶー－]+\z/
+  },on: :create
+  validates :birthday, presence: true
+
 end
