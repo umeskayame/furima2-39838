bd0e6b98a00f3252977508304240e3ccb9f2709d
umeskayame <ume122.0321@gmail.com>
2023-10-03 14:52:56 +0900

    修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,10 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name, format: {
-    with: /\A[ぁ-んァ-ヶ一\p{Han}]+\z/, message:"is invalid. Input full-width characters"
+    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message:"is invalid. Input full-width characters"
   },on: :create
   validates :first_name, format: {
-    with: /\A[ぁ-んァ-ヶ一\p{Han}]+\z/, message:"is invalid. Input full-width characters"
+    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message:"is invalid. Input full-width characters"
   },on: :create
   validates :kana_last_name, presence: {
     message:"is invalid. Input full-width katakana characters"
