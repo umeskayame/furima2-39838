7a5ac0d1473f68380c31a2a85d6981b01ac5f705
umeskayame <ume122.0321@gmail.com>
2023-10-04 18:17:02 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   has_one :purchase
   belongs_to :user
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
 end
