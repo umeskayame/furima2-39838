2faf168ebe03f1da260c30b1e7b19d50f5166761
umeskayame <ume122.0321@gmail.com>
2023-10-05 13:10:30 +0900

    newアクションの定義、ビュー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,12 @@
 class ItemsController < ApplicationController
   
+  def new
+    @item = Item.new
+  end
+  
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:item_name,:description,:category_id,:status_id,:shipping_fee_id,:prefecture_id,:shipping_day_id,:price,:user).merge(user_id: current_user.id)
   end
 end
