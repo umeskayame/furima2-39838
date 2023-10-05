4b445aba2b5dea386495470332cbb2c8186634d6
umeskayame <ume122.0321@gmail.com>
2023-10-05 16:24:26 +0900

    createアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
   
   private
 
