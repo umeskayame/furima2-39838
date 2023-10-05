3ebeeb19745372ce66db770b56ed1b70a522844c
umeskayame <ume122.0321@gmail.com>
2023-10-05 18:11:56 +0900

    正常系テスト実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,9 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  validates :image, presence: {
+    message:"can't be blank"
+  }
   validates :image, presence: true
   validates :item_name, presence: true
   validates :description, presence: true
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品の出品ができる場合' do
+      it '商品の出品ができるとき' do
+        expect(@item).to be_valid
+      end
+    end
+  end
 end
+
