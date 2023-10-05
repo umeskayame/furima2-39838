3f1f9e9271083cb341d05d45d962f5ad8d4c3ae2
umeskayame <ume122.0321@gmail.com>
2023-10-05 11:02:56 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,18 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                  format: { with: /\A[0-9]+\z/ }
+
+
+
+
 end
