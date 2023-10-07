44fdacc27dc675fe6c8300d02ab56229da83f154
umeskayame <ume122.0321@gmail.com>
2023-10-07 17:22:06 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create]
   
   def new
     @item = Item.new
@@ -16,6 +17,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image,:item_name,:description,:category_id,:status_id,:shipping_fee_id,:prefecture_id,:shipping_day_id,:price,:user).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:item_name,:description,:category_id,:status_id,:shipping_fee_id,:prefecture_id,:shipping_day_id,:price).merge(user_id: current_user.id)
   end
 end
