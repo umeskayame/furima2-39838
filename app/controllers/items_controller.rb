8d4ced82dbf98cd59cf0daedf33b471edebfe445
umeskayame <ume122.0321@gmail.com>
2023-10-08 20:38:04 +0900

    訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update]
-  before_action :contributor_confirmation , only: [:edit, :update]
+  before_action :authenticate_user! , only: [:new, :create,:edit, :update]
   
   def new
     @item = Item.new
@@ -25,12 +24,15 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.id != @item.user_id 
+      redirect_to root_path
+    end
   end
   
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to item_path(item.id)
+      redirect_to item_path(@item.id)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -41,11 +43,6 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image,:item_name,:description,:category_id,:status_id,:shipping_fee_id,:prefecture_id,:shipping_day_id,:price).merge(user_id: current_user.id)
   end
-  
-  def contributor_confirmation
-    @item = Item.find(params[:id])
-    unless current_user == @item.user
-    redirect_to root_path
-    end
-  end
 end
+
+
