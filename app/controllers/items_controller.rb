ffe94afe091372c033e1dcf109adeb40b41549f9
umeskayame <ume122.0321@gmail.com>
2023-10-09 14:40:20 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,8 @@ class ItemsController < ApplicationController
   def destroy
     if current_user.id == @item.user_id 
     @item.destroy
-    redirect_to root_path
     end
+    redirect_to root_path
   end
   
   private
