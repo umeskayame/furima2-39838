2de01a4c80bf7c18dc2f672510327fddfd9dce09
umeskayame <ume122.0321@gmail.com>
2023-10-13 16:46:24 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -52,19 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_105203) do
     t.index ["purchase_id"], name: "index_deliveries_on_purchase_id"
   end
 
-  create_table "deliverlies", charset: "utf8", force: :cascade do |t|
-    t.bigint "purchase_id", null: false
-    t.string "postcode", null: false
-    t.integer "prefecture_id", null: false
-    t.string "city", null: false
-    t.string "house_number", null: false
-    t.string "building"
-    t.string "phone", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["purchase_id"], name: "index_deliverlies_on_purchase_id"
-  end
-
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.text "description", null: false
@@ -110,7 +97,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_105203) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "deliveries", "purchases"
-  add_foreign_key "deliverlies", "purchases"
   add_foreign_key "items", "users"
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "users"
